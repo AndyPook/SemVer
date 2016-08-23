f25c739af9f98d2a4c588358635d629b7b534037
AndyPook <andy.pook@gmail.com>
2016-08-23 19:00:21 +0100

    testing appveyor

--- build.ps1
+++ build.ps1
@@ -1,13 +1,16 @@
 
 if(Test-Path .\artifacts) { Remove-Item .\artifacts -Force -Recurse }
 
+
+#$revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
+#$revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
+#$rev=(git rev-parse --short HEAD)
+
+$json = Get-Content -Raw -Path ".\src\SemVer\project.json" | ConvertFrom-Json
+$env:projectVersion = $json.version.trimend("*").trimend("-")
+$env:projectVersion
+
 dotnet restore
-
-$revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
-$revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
-$rev=(git rev-parse --short HEAD)
-
 dotnet test .\src\SemVer.Tests -c Release
-
 dotnet pack .\src\SemVer -c Release -o .\artifacts
-dotnet pack .\src\SemVer -c Release -o .\artifacts --version-suffix="git-$rev"
+#dotnet pack .\src\SemVer -c Release -o .\artifacts --version-suffix="git-$rev"
